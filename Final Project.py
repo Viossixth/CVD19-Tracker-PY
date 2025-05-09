a32038228b339fa6cccd945aa0f919706cca2df1
ViosSixth <risimac@outlook.com>
2025-05-09 22:33:32 +0200

    update

--- Final Project.py
+++ Final Project.py
@@ -7,13 +7,30 @@ import seaborn as sns
 #2.1 Load and explore Dataset
 df = pd.read_csv('owid-covid-data.csv')
 print(df.head())
-print(df.column())
-print(df.isnull().sum())
+#print(df.columns())
 
-#2.2 Data Cleaning
+#Missing Values
+df.isnull()
 
+#3.1 Data Cleaning
+#Filtering data to  show only South Africa as the location 
+filtered_df = df[df['location'] == 'South Africa']
+print(filtered_df)
 
-#Basic Data Analysis
+#Dropping rows with incomplete data
+df_dropped = df.dropna(how='all', axis = 1 )
+print(df_dropped)
+
+#Changing column name from date to datetime
+df = df.rename( columns={'date': 'datetime'})
+print(df)
+
+#Interpolation of missing data
+#df.interpolate(method='polynomial',)
+print(df.interpolate())
+
+#4 Basic Data Analysis
 print(df.describe())
 
-#Data Visualization 
+#Exploratory Data Analysis
+
